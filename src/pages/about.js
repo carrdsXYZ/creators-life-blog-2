8e92a4d29c7f0f6164067b151f1b6e0e68265057
Yoav Morami <yoav88@gmail.com>
2022-02-26 16:30:05 +0200

    about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,19 +19,8 @@ export default function About({data}) {
         <HeadingXL>About</HeadingXL>
         <Image fluid={data.RandomPhoto.childImageSharp.fluid} />
         <TextBody>
-          Building mr concerns servants in he outlived am breeding. He so lain
-          good miss when sell some at if. Told hand so an rich gave next. How
-          doubt yet again see son smart. While mirth large of on front. Ye he
-          greater related adapted proceed entered an. Through it examine express
-          promise no. Past add size game cold girl off how old.
-          <br />
-          <br />
-          Bringing so sociable felicity supplied mr. September suspicion far him
-          two acuteness perfectly. Covered as an examine so regular of. Ye
-          astonished friendship remarkably no. Window admire matter praise you
-          bed whence. Delivered ye sportsmen zealously arranging frankness
-          estimable as. Nay any article enabled musical shyness yet sixteen yet
-          blushes. Entire its the did figure wonder off.
+          I'm intrigued about online solopreneurs, the creator economy, and where it's heading. 
+          I'll use this blog as a platform for me to learn more about these topics.
         </TextBody>
         <Button href="mailto:your&#64;email.com">Get in touch</Button>
       </Layout>
